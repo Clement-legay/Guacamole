3f248c0195128aa11c37b646a17a4bee1c57c29e
Thibaut-Lecee <leceethibaut@gmail.com>
2022-06-02 11:31:37 +0200

    ajout migrations

---
+++ database/migrations/2022_06_02_092423_create_roles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('canCreate')->default(0);
+            $table->tinyInteger('canUpdate')->default(0);
+            $table->tinyInteger('canComment')->default(0);
+            $table->tinyInteger('canDelete')->default(0);
+            $table->tinyInteger('canDeleteVideo')->default(0);
+            $table->tinyInteger('canDeleteComment')->default(0);
+            $table->tinyInteger('canBanUser')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
