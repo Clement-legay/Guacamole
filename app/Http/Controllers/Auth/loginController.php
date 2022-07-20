f81db40a6830818f8a06657bafaba541c113f84c
Docteurexplod <delta80@outlook.fr>
2022-07-20 14:25:24 +0200

    update

--- app/Http/Controllers/Auth/loginController.php
+++ app/Http/Controllers/Auth/loginController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+use Tymon\JWTAuth\Payload;
+
 
 class loginController extends Controller
 {
@@ -24,7 +29,6 @@ class loginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            $id = Auth::user()->id;
             if (Auth::user()->email_verified_at) {
                 return redirect()->route('home');
             } else {
@@ -45,6 +49,7 @@ class loginController extends Controller
 
     public function loginAPI(Request $request)
     {
+
         $request->validate([
             'email' => 'required',
             'password' => 'required',
@@ -53,9 +58,21 @@ class loginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            $id = Auth::user()->id;
+            $user = JWTFactory::claims([
+                'sub' => Auth::user()->id,
+                'email' => Auth::user()->email,
+                'first_name' => Auth::user()->first_name,
+                'last_name' => Auth::user()->last_name,
+                'username' => Auth::user()->username,
+                'profile_picture' => Auth::user()->profile_picture,
+                'color' => Auth::user()->color,
+                'role' => Auth::user()->role()->id,
+                'iat' => time(),
+                'exp' => time() + (60 * 60 * 24 * 30),
+            ])->make();
+            $token = JWTAuth::encode($user);
             if (Auth::user()->email_verified_at) {
-                return response()->json(['success' => true, 'id' => $id, 'user' => Auth::user()]);
+                return response()->json(['success' => true, 'user' => $user]);
             } else {
                 Auth::logout();
                 return response()->json(['success' => false, 'error' => 'Email not verified']);
